c79ce862b11e8a8b6140b7867d73e0a8529301c4
ram <ramobjectsquare@gmail.com>
2026-05-07 14:44:05 +0530

    feat: replace background particles with a custom interactive rain effect component

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import CustomCursor from './components/CustomCursor';
-import BackgroundParticles from './components/BackgroundParticles';
+import RainEffect from './components/RainEffect';
 import usePortfolioScripts from './hooks/usePortfolioScripts';
 import './index.css';
 
@@ -46,7 +46,7 @@ function App() {
       )}
 
       <CustomCursor />
-      <BackgroundParticles />
+      <RainEffect />
       <div className="noise-overlay"></div>
 
       <Header />
